ff25c0effcff7eee5cd8792d39977a93016faccb
subhojit-mukherjee <subhojit_mukherjee_1988@yahoo.co.in>
2015-06-02 13:03:06 +0530

    Ds using Java

--- SegmentTree.java
+++ SegmentTree.java
@@ -103,34 +103,6 @@ public class SegmentTree {
 
 	}
 
-	/*
-	 * A recursive function to update the nodes which have the given index in
-	 * their range. The following are parameters st, index, ss and se are same
-	 * as getSumUtil() i --> index of the element to be updated. This index is
-	 * in input array. diff --> Value to be added to all nodes which have i in
-	 * range void updateValueUtil(int *st, int ss, int se, int i, int diff, int
-	 * index) { // Base Case: If the input index lies outside the range of this
-	 * segment if (i < ss || i > se) return;
-	 * 
-	 * // If the input index is in range of this node, then update the value //
-	 * of the node and its children st[index] = st[index] + diff; if (se != ss)
-	 * { int mid = getMid(ss, se); updateValueUtil(st, ss, mid, i, diff, 2*index
-	 * + 1); updateValueUtil(st, mid+1, se, i, diff, 2*index + 2); } }
-	 * 
-	 * // The function to update a value in input array and segment tree. // It
-	 * uses updateValueUtil() to update the value in segment tree void
-	 * updateValue(int arr[], int *st, int n, int i, int new_val) { // Check for
-	 * erroneous input index if (i < 0 || i > n-1) { printf("Invalid Input");
-	 * return; }
-	 * 
-	 * // Get the difference between new value and old value int diff = new_val
-	 * - arr[i];
-	 * 
-	 * // Update the value in array arr[i] = new_val;
-	 * 
-	 * // Update the values of nodes in segment tree updateValueUtil(st, 0, n-1,
-	 * i, diff, 0); }
-	 */
 	void updateValueUtil(int segArr[], int ss, int se, int i, int diff,
 			int index) {
 		// Base Case: If the input index lies outside the range of this segment
